efdd98114cdf49a087eb770c6a7d3b04bc6d8497
mcquin <mcquin@broadinstitute.org>
2017-12-06 16:48:06 -0500

    Test option format_broad_cmap

--- tests/testthat/test-annotate.R
+++ tests/testthat/test-annotate.R
@@ -17,3 +17,31 @@ test_that("`annotate` adds plate and well metadata", {
 
   file.remove(annotated_csv)
 })
+
+test_that("`annotate` with format_broad_cmap", {
+  annotated_csv <- tempfile("SQ00015116_augmented.csv")
+
+  annotate("batch0", "SQ00015116",
+           cell_id = "abc-456-zyx",
+           format_broad_cmap = TRUE,
+           output = annotated_csv,
+           workspace_dir = system.file("extdata", package = "cytotools"))
+
+  result <- readr::read_csv(annotated_csv)
+
+  # Assert these columns were added:
+  broad_cmap_colnames = c(
+    "Metadata_pert_id",
+    "Metadata_pert_mfc_id",
+    "Metadata_pert_well",
+    "Metadata_pert_id_vendor",
+    "Metadata_cell_id",
+    "Metadata_broad_sample_type",
+    "Metadata_pert_vehicle",
+    "Metadata_pert_type"
+  )
+
+  expect_true(all(broad_cmap_colnames %in% colnames(result)))
+
+  file.remove(annotated_csv)
+})
