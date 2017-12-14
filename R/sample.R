0781ad2625c386d30738c6200ec71f2f21120628
mcquin <mcquin@broadinstitute.org>
2017-12-14 12:52:13 -0500

    Implement sample

---
+++ R/sample.R
@@ -0,0 +1,88 @@
+#' **DESCRIPTION**
+#'
+#' \code{annotate} **DESCRIPTION**
+#'
+#' @param batch_id        Batch ID.
+#' @param pattern         Regular expression - only CSVs.
+#' @param output          Output file - either CSV or RDS.
+#' @param replicates      Number of replicates to select per plate map.
+#' @param workspace_dir   Root directory containing backend and metadata subdirectories. Can be relative or absolute. default: \code{"."}.
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
+#' @importFrom magrittr extract2
+#' @importFrom rlang .data
+#' @importFrom utils head tail
+#' @export
+sample <- function(batch_id, pattern, output,
+                   replicates = NULL,
+                   workspace_dir = ".") {
+
+  backend_dir <- paste(workspace_dir, "backend", batch_id, sep = "/")
+
+  metadata_dir <- paste(workspace_dir, "metadata", batch_id, sep = "/")
+
+  file_list <- list.files(backend_dir,
+                          pattern = pattern,
+                          recursive = T, full.names = T)
+
+  if (!is.null(replicates)) {
+    # get the list of plates that retrieved using the pattern
+    plate_list_retrieved <-
+      lapply(file_list,
+             function(file) {
+               head(tail(stringr::str_split(file, "/")[[1]], 2), 1)
+             }) %>%
+      unlist() %>%
+      dplyr::data_frame(Assay_Plate_Barcode = .data$.)
+
+    replicates <- as.integer(replicates)
+
+    # create a plate_list based on number of replicates to be selected
+    plate_list <-
+      suppressMessages(
+        readr::read_csv(
+          paste(metadata_dir, "barcode_platemap.csv", sep = "/"),
+          col_types = readr::cols(Assay_Plate_Barcode = readr::col_character(),
+                                  Plate_Map_Name = readr::col_character()))) %>%
+      dplyr::select("Assay_Plate_Barcode", "Plate_Map_Name") %>%
+      dplyr::inner_join(plate_list_retrieved, by = "Assay_Plate_Barcode") %>%
+      dplyr::group_by("Plate_Map_Name") %>%
+      dplyr::arrange("Assay_Plate_Barcode") %>%
+      dplyr::mutate(
+        replicate_id = dplyr::dense_rank(.data$Assay_Plate_Barcode)) %>%
+      dplyr::filter(replicate_id %in% seq(replicates)) %>%
+      dplyr::ungroup() %>%
+      dplyr::select("Assay_Plate_Barcode") %>%
+      magrittr::extract2("Assay_Plate_Barcode")
+
+    # filter file_list based on plate_list
+    file_list <-
+      lapply(file_list, function(file) {
+        if (length(unlist(lapply(plate_list,
+                                 function(plate) grep(plate, file))))) {
+          file
+        }}) %>%
+      unlist()
+  }
+
+  futile.logger::flog.info(
+    sprintf("Reading %d files...:\n%s", length(file_list),
+            paste(file_list, collapse = "\n"))
+    )
+
+  df <- file_list %>%
+    lapply(function(x) suppressMessages(readr::read_csv(x))) %>%
+    dplyr::bind_rows()
+
+  futile.logger::flog.info(sprintf("Output contains %d rows.", nrow(df)))
+
+  if (tools::file_ext(output) == "rds") {
+    saveRDS(df, output)
+  } else if (tools::file_ext(output) == "csv"){
+    readr::write_csv(df, output)
+#   } else if (tools::file_ext(output) == "feather"){
+#     feather::write_feather(df, output)
+  } else {
+    stop(paste0("Unsupported file extension: ", tools::file_ext(output)))
+  }
+}
