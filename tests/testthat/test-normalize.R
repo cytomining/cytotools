77692cd60eca33a84ec14d4668c8099c66055a5a
Shantanu Singh <shsingh@broadinstitute.org>
2020-03-26 01:05:59 -0400

    Add a normalization test - still failing

---
+++ tests/testthat/test-normalize.R
@@ -0,0 +1,76 @@
+context("normalize")
+
+test_that("`normalize` normalizes data", {
+  futile.logger::flog.threshold(futile.logger::WARN)
+
+  normalized_csv <- tempfile("SQ00015116_normalized.csv")
+
+  aggregated_csv <-
+    system.file(
+      "extdata", "backend", "batch0", "SQ00015116",
+      "SQ00015116_augmented.csv",
+      package = "cytotools"
+    )
+
+  normalize(input_file = aggregated_csv,
+            output_file = normalized_csv,
+            operation = "robustize",
+            subset = "Metadata_broad_sample_type == 'control'")
+
+  expected_csv <-
+    system.file(
+      "extdata", "backend", "batch0", "SQ00015116",
+      "SQ00015116_normalized.csv",
+      package = "cytotools"
+    )
+
+  expected <- readr::read_csv(expected_csv)
+
+  actual <- readr::read_csv(normalized_csv)
+
+  expect_equal(actual, expected)
+
+  file.remove(normalized_csv)
+})
+
+test_that("`normalize` normalizes data sampling from single cell data", {
+  futile.logger::flog.threshold(futile.logger::WARN)
+
+  normalized_csv <- tempfile("SQ00015116_normalized_single_cell.csv")
+
+  aggregated_csv <-
+    system.file(
+      "extdata", "backend", "batch0", "SQ00015116",
+      "SQ00015116_augmented.csv",
+      package = "cytotools"
+    )
+
+  sqlite_file <-
+    system.file(
+      "extdata", "backend", "batch0", "SQ00015116",
+      "SQ00015116.sqlite",
+      package = "cytotools"
+    )
+
+  normalize(input_file = aggregated_csv,
+            sample_single_cell = TRUE,
+            input_sqlite_file = sqlite_file,
+            output_file = normalized_csv,
+            operation = "robustize",
+            subset = "Metadata_broad_sample_type == 'control'")
+
+  expected_csv <-
+    system.file(
+      "extdata", "backend", "batch0", "SQ00015116",
+      "SQ00015116_normalized_single_cell.csv",
+      package = "cytotools"
+    )
+
+  expected <- readr::read_csv(expected_csv)
+
+  actual <- readr::read_csv(normalized_csv)
+
+  expect_equal(actual, expected)
+
+  file.remove(normalized_csv)
+})
