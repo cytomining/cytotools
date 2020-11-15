cc445a1d7355bc4a65ba4263e07ea5e0519e8243
Shantanu Singh <shsingh@broadinstitute.org>
2020-11-15 13:23:43 -0500

    Update aggregate.R

--- R/aggregate.R
+++ R/aggregate.R
@@ -67,7 +67,7 @@ aggregate <- function(sqlite_file,
     purrr::map(aggregate_objects) %>%
     purrr::reduce(dplyr::inner_join, by = strata)
 
-  futile.logger::flog.info(paste0("Started aggregating images ..."))
+    futile.logger::flog.info(paste0("Started aggregating ", image_table, " ..."))
 
   if(!is.null(image_variables)) {
     aggregate_images <-
