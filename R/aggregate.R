55b07c4fec26fbc03b57cdc81ec90d26ee6d4c37
Shantanu Singh <shsingh@broadinstitute.org>
2020-11-15 13:24:11 -0500

    Update aggregate.R

--- R/aggregate.R
+++ R/aggregate.R
@@ -50,7 +50,7 @@ aggregate <- function(sqlite_file,
         )
     }
 
-    futile.logger::flog.info(paste0("Started aggregating ", compartment, " ..."))
+    futile.logger::flog.info(paste0("Started aggregating ", compartment))
 
     cytominer::aggregate(
       population = object,
@@ -67,7 +67,7 @@ aggregate <- function(sqlite_file,
     purrr::map(aggregate_objects) %>%
     purrr::reduce(dplyr::inner_join, by = strata)
 
-    futile.logger::flog.info(paste0("Started aggregating ", image_table, " ..."))
+    futile.logger::flog.info(paste0("Started aggregating ", image_table))
 
   if(!is.null(image_variables)) {
     aggregate_images <-
